e4b59ee3566e62c4a512d05ac0d2afb7ee90d04b
anning <864399407@qq.com>
2024-04-07 14:31:00 +0800

    no message

--- start.bat
+++ start.bat
@@ -1,19 +1,21 @@
 @echo off
+chcp 65001
 if not exist ".venv" (
-    echo 创建虚拟环境...
-    python -m venv .venv
+    echo 测试测试
 ) else (
-    echo 虚拟环境已存在，无需创建。
+    echo 测试测试测试
 )
 
 echo 激活虚拟环境...
 call .venv\Scripts\activate
+
 
 echo 安装依赖...
 pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 
 echo 启动程序...
 python main.py
+
 
 echo 运行完成，按任意键退出。
 pause > nul
